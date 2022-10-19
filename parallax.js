6788fe9cd9046897cb0a697db4513f883a11e0b6
Federico Barbieri <federicobarbieri54@gmail.com>
2022-10-19 13:27:48 +0200

    change slider

--- parallax.js
+++ parallax.js
@@ -5,7 +5,7 @@ const ctx = canvas.getContext('2d');
 const CANVAS_WIDTH = canvas.width = 800;
 const CANVAS_HEIGHT = canvas.height = 700;
 
-let gameSpeed = 5;
+let gameSpeed = 10;
 
 const backgroundLayer1 = new Image();
 backgroundLayer1.src = 'layers/layer-1.png';
@@ -21,6 +21,17 @@ backgroundLayer4.src = 'layers/layer-4.png';
 
 const backgroundLayer5 = new Image();
 backgroundLayer5.src = 'layers/layer-5.png';
+
+const slider = document.getElementById('slider');
+slider.value = gameSpeed;
+
+const showGameSpeed = document.getElementById('showGameSpeed');
+showGameSpeed.innerHTML = gameSpeed;
+
+slider.addEventListener('change', function(e) {
+    gameSpeed = e.target.value;
+    showGameSpeed.innerHTML = e.target.value;
+});
 
 class Layer {
     constructor(image, speedModifier){
